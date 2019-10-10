664478da57da096a928bd099d381fafbd78d176b
nrkohan <55960545+nrkohan@users.noreply.github.com>
2019-10-09 23:55:58 -0400

    Update Database.java

--- Database.java
+++ Database.java
@@ -18,7 +18,7 @@ public class Database {
         
     }
     
-    public Integer searchPetName(String animalName, String lastName){
+    public Integer searchPetName(String animalName, String lastName, String operator){
         return 1;
     }
     
@@ -42,17 +42,77 @@ public class Database {
         return "Doe";
     }
     
+    public String getGender(Integer animalID){
+        return "Male";
+    }
+    
+    public String getWeight(Integer animalID){
+        return "55.3";
+    }
+    
     public Integer postAnimal(String petName, String lastName, String firstName,
                               String Gender, String weight){
         
         return 1;
     }
     
-    public void postMedication(Integer animalID, String[] medList){
+    public void updateAnimal(Integer animalID, String weight){
         
     }
     
-    public void postShots(Integer animalID, String[] shotList){
+    public String[][] postMedication(Integer animalID, String medication, String medicationInfo){
+        //Post the new medication then return back a list of known medications
         
+        String[][] medData = { 
+            { "Heartgard", "Once a month to prevent heartworm" },
+            { "Aspirin Powder", "Apple flavor" }
+        }; 
+        
+        return medData;
+        
+    }
+    
+    public String[][] postShots(Integer animalID, String shotList, String shotDate){
+        
+        //Post my shot
+        
+        //Now return a list of all known shots
+        String[][] shotData = { 
+            { "9-3-18", "Rabies" },
+            { "9-3-18", "Distemper" }
+        }; 
+        
+        return shotData;
+        
+    }
+    
+    public String[][] getShots(Integer animalID){
+        
+        //Now return a list of all known shots
+        String[][] shotData = { 
+            { "9-3-18", "Rabies" },
+            { "9-3-18", "Distemper" }
+        }; 
+        
+        return shotData;
+    }
+    
+    public String[][] getMeds(Integer animalID){
+        String[][] medData = { 
+            { "Heartgard", "Once a month to prevent heartworm" },
+            { "Aspirin Powder", "Apple flavor" }
+        }; 
+        
+        return medData;
+        
+    }
+    
+    public String[][] getAppt(Integer animalID){
+        String[][] apptData = { 
+            { "9-3-19", "9:30am", "30" },
+            { "9-12-19", "10:00am", "15" }
+        };
+        
+        return apptData;
     }
 }
